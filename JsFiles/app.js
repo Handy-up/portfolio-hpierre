baa40b1950b213213db1353539a9d717a3a7d85f
Handy Charles G. Pierre <hpierre@dept-info.crosemont.quebec>
2024-11-24 23:15:04 -0500

    Initialisation de mon portfolio version 0.1

---
+++ JsFiles/app.js
@@ -0,0 +1,8 @@
+// Initialise le popover pour les éléments avec la classe .popover-dismiss
+const popover = new bootstrap.Popover('.message-popover', {
+    trigger: 'focus'
+});
+
+const popover = new bootstrap.Popover('.message-popover2', {
+    trigger: 'focus'
+});
