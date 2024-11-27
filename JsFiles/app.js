c11250f87ac36ee24b538a1c1627b7c0f1f1b650
Handy Charles G. Pierre <hpierre@dept-info.crosemont.quebec>
2024-11-26 20:10:36 -0500

    Update responsive

--- JsFiles/app.js
+++ JsFiles/app.js
@@ -1,8 +1,26 @@
-// Initialise le popover pour les éléments avec la classe .popover-dismiss
-const popover = new bootstrap.Popover('.message-popover', {
-    trigger: 'focus'
+
+document.getElementById('menu-toggler').addEventListener('click', function () {
+    const menu = document.getElementById('navbar-menu');
+    menu.classList.toggle('active');
 });
 
-const popover = new bootstrap.Popover('.message-popover2', {
-    trigger: 'focus'
+
+// Ajout d'un gestionnaire d'événements pour le défilement
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 0) {
+        navbar.classList.remove("transparent");
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+        navbar.classList.add("transparent");
+    }
+});
+
+// Initialisation de la classe au chargement de la page
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY === 0) {
+        navbar.classList.add("transparent");
+    }
 });
